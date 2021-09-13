17edb6c5106104e7edb3c4dab26a6c8e6cd7b485
Matthew Ward <matthewward@granular.ag>
2021-09-12 23:04:49 -0700

    add heroku file

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,6 @@
-export const hostname = process.env.MONGO_HOSTNAME || 'localhost';
-export const mongo_port = process.env.MONGO_PORT || '27017';
-export const mongo_db = process.env.MONGO_DB || 'test_database';
-export const mongo_url = `mongodb://${hostname}:${mongo_port}/${mongo_db}`;
+export const hostname = process.env.MONGO_HOSTNAME || "localhost";
+export const mongo_port = process.env.MONGO_PORT || "27017";
+export const mongo_db = process.env.MONGO_DB || "test_database";
+export const mongo_url =
+  process.env.MONGO_DB_URL || `mongodb://${hostname}:${mongo_port}/${mongo_db}`;
 export const port = process.env.PORT || 8080;
